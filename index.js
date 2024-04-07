911852602dd4cf5818288d107b3aa0afdc2409c6
Faiza Kashaf <faizakashaf4@gmail.com>
2024-04-07 14:27:58 +0500

    resolve issue of scroll arrow on home page

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // --------- Home to About Scroll Arrow ---------
-const aboutSection = document.getElementById("about");
-const scrollDownArrow = document.querySelector(".arrow_down");
 
-function scrollDowntoAbout() {
-    scrollDownArrow.addEventListener("click", function() {
-        aboutSection.scrollIntoView({ behavior: 'smooth' });
-    });
-}
-scrollDowntoAbout();
+document.addEventListener("DOMContentLoaded", function() {
+    const aboutSection = document.getElementById("about");
+    const scrollDownArrow = document.querySelector(".arrow_down");
+    
+    function scrollDowntoAbout() {
+        scrollDownArrow.addEventListener("click", function() {
+            aboutSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    }
+    scrollDowntoAbout();
+    
+});
