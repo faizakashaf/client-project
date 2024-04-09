f0b6c89697594ed837d255ea6896a3013e416b5e
Faiza Kashaf <faizakashaf4@gmail.com>
2024-04-09 16:55:33 +0500

    popup stying checking

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// --------- Home to About Scroll Arrow ---------
+// --------- Home to About Scroll Arrow ---------//
 
 document.addEventListener("DOMContentLoaded", function() {
     const aboutSection = document.getElementById("about");
@@ -13,3 +13,26 @@ document.addEventListener("DOMContentLoaded", function() {
     
 });
 
+//--------For Page Pop Up ----------//
+
+const previewContainer = document.querySelector('.preview_container');
+const insidePreview = document.querySelector(".inside_preview");
+const body = document.querySelector("body");
+
+
+const galleryImages = document.querySelectorAll('.thumbnail img');
+galleryImages.forEach(img => {
+    img.addEventListener('click', showPreviewContainer);
+});
+
+function showPreviewContainer() {
+    previewContainer.style.display = 'block';
+    body.style.overflow = "hidden";
+}
+
+//-----Pop Up clsing Working -----------//
+const popupPageCloseButton = document.querySelector(".close_icon");
+popupPageCloseButton.addEventListener("click", () => {
+previewContainer.style.display = "none";
+body.style.overflow = "auto";
+})
